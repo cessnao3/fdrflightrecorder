a4c2a02ae2e617d7e723b37794fdebdac192e792
Ian O'Rourke <cessna.ian@gmail.com>
2015-05-19 23:35:32 -0400

    Minor Updates

--- app/src/main/java/com/ianorourke/fdrflightrecorder/PilotFragment.java
+++ app/src/main/java/com/ianorourke/fdrflightrecorder/PilotFragment.java
@@ -1,19 +1,14 @@
 package com.ianorourke.fdrflightrecorder;
 
-import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
-
-import java.util.ArrayList;
 
 public class PilotFragment extends Fragment {
 
